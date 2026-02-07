47f1f5930f2b7028624f2aad0c31285f714be5f9
Aaron776 <68518337+Aaron776@users.noreply.github.com>
2026-02-07 18:41:57 -0500

    correccion

--- config/oauth_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Configuración de OAuth para GitHub y Facebook
-define('GITHUB_CLIENT_ID', 'Ov23lifjnaK1bHrQpcKP');
-define('GITHUB_CLIENT_SECRET', 'ab5e4dfeab5c466f0de5abe47219afca0b1ea2ab');
-define('GITHUB_REDIRECT_URI', 'http://localhost/Sistemas_Web_PHP/Sistema_Gestion_Gimnasio/controladores/github_callback.php');
-
-define('FACEBOOK_APP_ID', '1400678928112493');
-define('FACEBOOK_APP_SECRET', 'ecf37e5a63c97defef059ccfe92d4c1c');
-define('FACEBOOK_REDIRECT_URI', 'http://localhost/Sistemas_Web_PHP/Sistema_Gestion_Gimnasio/controladores/facebook_callback.php');
